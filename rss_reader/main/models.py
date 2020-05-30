2021645905d34d2216bdd4134c94f0294df69294
natarajan1993 <natraj_93@yahoo.com>
2020-05-29 20:10:14 -0500

    String representation for Feed model

--- rss_reader/main/models.py
+++ rss_reader/main/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Feed(models.Model):
     name = models.CharField(max_length=50)
     url = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
